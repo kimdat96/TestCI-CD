a3aac4752e01f93d03c83ae2e5105a8db34aa532
kimdat96 <dat.pham@2359media.com>
2020-11-05 22:31:55 +0700

    on: push

--- CI-CD/CI-CD/Controllers/HomeController.cs
+++ CI-CD/CI-CD/Controllers/HomeController.cs
@@ -11,7 +11,6 @@ namespace CI_CD.Controllers
 {
     public class HomeController : Controller
     {
-
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
